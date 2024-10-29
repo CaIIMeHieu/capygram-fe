b3a10f22244093471a86e1c0e40c9607a564b13d
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2024-10-29 15:18:32 +0700

    add request header to allow ngrok

--- src/utils/axios-http/axios-http.js
+++ src/utils/axios-http/axios-http.js
@@ -1,47 +1,51 @@
 /* eslint-disable */
-import {  refreshToken } from '@/api/authApi/auth';
-import axios from 'axios';
+import { refreshToken } from "@/api/authApi/auth";
+import axios from "axios";
 
 //tao ra instance axios cho cac request khong can token
 const createAxiosInstance = (baseURL) => {
   return axios.create({
     baseURL,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
   });
 };
 
 const createAuthInstance = (baseURL) => {
   const instance = axios.create({
     baseURL,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
   });
 
   instance.interceptors.response.use(
-    (response) => response, async (error) => {
-
-      if (error.response.status === 500 ) {
-        
+    (response) => response,
+    async (error) => {
+      if (error.response.status === 500) {
         try {
           const data = await refreshToken();
-          
-          console.log(data);
-          localStorage.setItem('accessToken', data.accessToken);
-          localStorage.setItem('refreshToken', data.refreshToken);
 
-          //gắn accessToken cho instance
-          instance.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
-          //thuc hien request bi loi
+          console.log(data);
+          localStorage.setItem("accessToken", data.accessToken);
+          localStorage.setItem("refreshToken", data.refreshToken);
+
+          // gắn accessToken cho instance
+          instance.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${data.accessToken}`;
+          //thực hiện request bị lỗi
           return instance;
         } catch (error) {
           console.error(error);
-          //truong hop khong refresh duoc token
-          // localStorage.removeItem('accessToken');
-          // localStorage.removeItem('refreshToken');
           return Promise.reject(error);
         }
       }
       return Promise.reject(error);
     }
   );
-  
+
   return instance;
 };
 
@@ -49,19 +53,42 @@ const publicInstance = createAxiosInstance(import.meta.env.VITE_APP_URL_BE);
 const authInstance = createAuthInstance(import.meta.env.VITE_APP_URL_BE);
 const postInstance = createAuthInstance(import.meta.env.VITE_APP_URL_BE_POST);
 const graphInstance = createAuthInstance(import.meta.env.VITE_APP_URL_BE_GRAPH);
-const newsFeedInstance = createAuthInstance(import.meta.env.VITE_APP_URL_BE_NEWSFEED);
+const newsFeedInstance = createAuthInstance(
+  import.meta.env.VITE_APP_URL_BE_NEWSFEED
+);
 const chatInstance = createAuthInstance(import.meta.env.VITE_APP_URL_BE_CHAT);
 
 const request = (instance, config) => {
-  return instance({ ...config });
+  return instance({
+    ...config,
+    headers: {
+      "ngrok-skip-browser-warning": "69420",
+    },
+  });
 };
 
 const requestWithToken = (instance, config) => {
-  const accessToken = localStorage.getItem('accessToken');
+  const accessToken = localStorage.getItem("accessToken");
   if (!accessToken) {
-    throw new Error('Bạn cần phải đăng nhập để thực hiện chức năng này!');
+    throw new Error("Bạn cần phải đăng nhập để thực hiện chức năng này!");
   }
-  return instance({ ...config, headers: { Authorization: `Bearer ${accessToken}` } });
+  return instance({
+    ...config,
+    headers: {
+      ...config.headers,
+      Authorization: `Bearer ${accessToken}`,
+      "ngrok-skip-browser-warning": "69420", // Thêm header ở đây
+    },
+  });
 };
 
-export { request, requestWithToken, authInstance, publicInstance, postInstance, graphInstance, newsFeedInstance, chatInstance };
+export {
+  request,
+  requestWithToken,
+  authInstance,
+  publicInstance,
+  postInstance,
+  graphInstance,
+  newsFeedInstance,
+  chatInstance,
+};
